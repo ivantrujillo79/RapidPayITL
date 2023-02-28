8ee99865a18443731ccf1cae92c0094c926e954a
Ivan Trujillo Lopez <ivan.trujillo@distillery.con>
2023-02-28 11:14:25 -0600

    Project's default controllers were removed Card Mangement Controller Created

---
+++ RapidPayITL/Controllers/CardManagementController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RapidPayITL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardManagementController : ControllerBase
+    {
+        public CardManagementController() { }
+
+        [HttpGet]
+        public async Task<IActionResult> Balance(string cardNumber)
+        {
+            return Ok();
+        }
+    }
+}
