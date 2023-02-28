bf705047f17df6db3783736cd5801418d8b00466
Ivan Trujillo Lopez <ivan.trujillo@distillery.con>
2023-02-28 11:26:36 -0600

    Database context creation Singularization coding standard

---
+++ RapidPayITL/Data/RapidPayDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RapidPayITL.Data
+{
+    public class RapidPayDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("RapidPayDB");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
